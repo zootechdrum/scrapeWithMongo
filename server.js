2d70e59480cb519742a1cdb93fe4f469c3e10b41
Cesar Gomez <zootechdrum@gmail.com>
2020-01-31 18:55:19 -0800

    extracted image

--- server.js
+++ server.js
@@ -3,37 +3,30 @@ var cheerio = require("cheerio");
 // Makes HTTP request for HTML page
 var axios = require("axios");
 
-// First, tell the console what server.js is doing
-console.log("\n***********************************\n" +
-            "Grabbing every thread name and link\n" +
-            "from reddit's webdev board:" +
-            "\n***********************************\n");
 
-// Making a request via axios for reddit's "webdev" board. The page's HTML is passed as the callback's third argument
+
 axios.get("https://www.cracked.com/").then(function(response) {
 
-  // Load the HTML into cheerio and save it to a variable
-  // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+
   var $ = cheerio.load(response.data);
 
   // An empty array to save the data that we'll scrape
   var results = [];
-  // $('a[title]','.content-cards-info')
-  // With cheerio, find each p-tag with the "title" class
-  // (i: iterator. element: the current element)
-      $('h3').children('a[title]','.content-cards-info').each(function(i, element) {
 
-    // Save the text of the element in a "title" variable
-    var title = $(element).text();
 
-    // In the currently selected element, look at its child elements (i.e., its a-tags),
-    // then save the values for any "href" attributes that the child elements may have
-    var link = $(element).attr("href");
+$('.content-cards-wrapper').each(function(i, element) {
+  
+
+
+    var title = $(element).children('a[title]').val()
+   // Save the text of the element in a "title" variable
+    var image = $('.content-cards-image').attr("data-original");
+
 
     // Save these results in an object that we'll push into the results array we defined earlier
     results.push({
       title: title,
-      link: link
+      image: image,
     });
   });
 
