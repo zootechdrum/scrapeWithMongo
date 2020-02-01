d450913301c0d6bf2d05fc18ad53bf453c3b10c0
Cesar Gomez <zootechdrum@gmail.com>
2020-02-01 13:48:27 -0800

    added morgan and added models index file

--- server.js
+++ server.js
@@ -1,37 +1,102 @@
-// Parses our HTML and helps us find elements
-var cheerio = require("cheerio");
-// Makes HTTP request for HTML page
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
 var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
+var PORT = 3000;
+
+// Initialize Express
+var app = express();
+
+// Configure middleware
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+
+mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
 
 
 
-axios.get("https://www.cracked.com/").then(function(response) {
 
 
-  var $ = cheerio.load(response.data);
 
-  // An empty array to save the data that we'll scrape
-  var results = [];
+// An empty array to save the data that we'll scrape
 
 
-$('.content-cards-wrapper').each(function(i, element) {
+app.get("/scrape", function (req, res) {
+  axios.get("https://www.cracked.com/").then(function (response) {
+    var $ = cheerio.load(response.data);
+
+    $('.content-cards-wrapper').each(function (i, element) {
+      var result = {};
+
+      // Grabs the title, image and link from cracked
+      var title = $(element).children('.content-cards-info').text()
+      var link = $(element).children().attr('href')
+      var image = $(element).children().attr("data-original");
+
+
+      // Save these results in an object that we'll push into the results array we defined earlier
+      if (link !== undefined && image !== undefined && title !== undefined) {
+        result.title = title,
+          result.link = link,
+          result.image = image
+      }
+
+      db.Article.create(result)
+        .then(function (dbArticle) {
+          // View the added result in the console
+          console.log(dbArticle);
+        })
+        .catch(function (err) {
+          // If an error occurred, log it
+          console.log(err);
+        });
+    });
   
-// Grabs the title, image and link from cracked
-    var title = $(element).children('.content-cards-info').text()
-    var link = $(element).children().attr('href')
-    var image = $(element).children().attr("data-original");
+  res.send("Scrape Complete");
+  })
+})
 
-
-    // Save these results in an object that we'll push into the results array we defined earlier
-    if(link !== undefined && image !== undefined && title !== undefined){
-      results.push({
-        link: link,
-        image: image,
-        title: title
-      });
-    }
-  });
-
-  // Log the results once you've looped through each of the elements found with cheerio
-  console.log(results);
-});
+//   app.get("/scrape", function(req, res) {
+//     // First, we grab the body of the html with axios
+//     axios.get("http://www.echojs.com/").then(function(response) {
+//       // Then, we load that into cheerio and save it to $ for a shorthand selector
+//       var $ = cheerio.load(response.data);
+  
+//       // Now, we grab every h2 within an article tag, and do the following:
+//       $("article h2").each(function(i, element) {
+//         // Save an empty result object
+//         var result = {};
+  
+//         // Add the text and href of every link, and save them as properties of the result object
+//         result.title = $(this)
+//           .children("a")
+//           .text();
+//         result.link = $(this)
+//           .children("a")
+//           .attr("href");
+  
+//         // Create a new Article using the `result` object built from scraping
+//         db.Article.create(result)
+//           .then(function(dbArticle) {
+//             // View the added result in the console
+//             console.log(dbArticle);
+//           })
+//           .catch(function(err) {
+//             // If an error occurred, log it
+//             console.log(err);
+//           });
+//       });
+  
+//       // Send a message to the client
+//       res.send("Scrape Complete");
+//     });
+//   });
+// }
