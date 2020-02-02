1439888251a11ac058310882770843baf356e408
Cesar Gomez <zootechdrum@gmail.com>
2020-02-02 12:30:04 -0800

    added get route for articles on server.js

--- server.js
+++ server.js
@@ -64,6 +64,20 @@ app.get("/scrape", function (req, res) {
   })
 })
 
+// Route for getting all Articles from the db
+app.get("/articles", function(req, res) {
+  // Grab every document in the Articles collection
+  db.Article.find({})
+    .then(function(dbArticle) {
+      // If we were able to successfully find Articles, send them back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
 // Start the server
 app.listen(PORT, function() {
   console.log("App running on port " + PORT + "!");
