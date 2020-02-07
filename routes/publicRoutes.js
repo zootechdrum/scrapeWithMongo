05013aa6ce0987d150f843567a0c88f40536b57d
Cesar Gomez <zootechdrum@gmail.com>
2020-02-07 05:56:46 -0800

    removed podcast from blog list

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -12,72 +12,75 @@ module.exports = function (app) {
         });
     });
 
-app.get("/scrape", function (req, res) {
-    axios.get("https://www.cracked.com/").then(function (response) {
-      var $ = cheerio.load(response.data);
-  
-      $('.content-cards-wrapper').each(function (i, element) {
-        var result = {};
-  
-        // Grabs the title, image and link from cracked
-        var title = $(this).children('.content-cards-info').children('h3').children('a').text()
-        var link = $(this).children('a').attr('href')
-        var image = $(this).children('a').attr('data-original')
-  
-        // Save these results in an object that we'll push into the results array we defined earlier
-        if (link !== undefined && image !== undefined && title !== undefined) {
-            result.title = title,
-            result.link = link,
-            result.image = image,
-  
-        //Second call to axios based on link parameter in results object
-        axios.get(result.link).then(function (response) {
-  
-          var $ = cheerio.load(response.data);
-  
-          $("meta[name='description']").each(function (i, element) {
-  
-            var description = $(this).attr("content")
-  
-            result.description = description
-  
-  
-            db.Article.insertMany(result)
-              .then(function (dbArticle) {
-                // View the added result in the console
-                console.log(dbArticle);
-              })
-              .catch(function (err) {
-                // If an error occurred, log it
-                console.log(err);
-              });
+    app.get("/scrape", function (req, res) {
+        axios.get("https://www.cracked.com/").then(function (response) {
+            var $ = cheerio.load(response.data);
+
+            $('.content-cards-wrapper').each(function (i, element) {
+                var result = {};
+
+                // Grabs the title, image and link from cracked
+                var title = $(this).children('.content-cards-info').children('h3').children('a').text()
+                var link = $(this).children('a').attr('href')
+                var image = $(this).children('a').attr('data-original')
+                var podcast = $(this).hasClass("content-cards-podcast")
+
+
+
+                // Save these results in an object that we'll push into the results array we defined earlier
+                if (link !== undefined && image !== undefined && title !== undefined && podcast === false) {
+                    result.title = title,
+                        result.link = link,
+                        result.image = image,
+
+                        //Second call to axios based on link parameter in results object
+                        axios.get(result.link).then(function (response) {
+
+                            var $ = cheerio.load(response.data);
+
+                            $("meta[name='description']").each(function (i, element) {
+
+                                var description = $(this).attr("content")
+
+                                result.description = description
+
+
+                                db.Article.insertMany(result)
+                                    .then(function (dbArticle) {
+                                        // View the added result in the console
+                                        console.log(dbArticle);
+                                    })
+                                    .catch(function (err) {
+                                        // If an error occurred, log it
+                                        console.log(err);
+                                    });
+                            })
+                        })
+
+                }
+            });
+
+            function redi() {
+                res.redirect("/")
+            }
+
+            setTimeout(redi, 5000);
         })
-        })
-  
-      }
-      });
-  
-      function redirect() {
-        res.redirect("/")
-      }
-      
-      setTimeout(myFunc, 5000);
+
     })
 
-  })
-  
-  // Route for getting all Articles from the db
-  app.get("/articles", function (req, res) {
-    // Grab every document in the Articles collection
-    db.Article.find({})
-      .then(function (dbArticle) {
-        // If we were able to successfully find Articles, send them back to the client
-        res.json(dbArticle);
-      })
-      .catch(function (err) {
-        // If an error occurred, send it to the client
-        res.json(err);
-      });
-  });
+    // Route for getting all Articles from the db
+    app.get("/articles", function (req, res) {
+        // Grab every document in the Articles collection
+        db.Article.find({})
+            .then(function (dbArticle) {
+                // If we were able to successfully find Articles, send them back to the client
+                res.json(dbArticle);
+            })
+            .catch(function (err) {
+                // If an error occurred, send it to the client
+                res.json(err);
+            });
+    });
 
 }
