15098fe47c4ec62d63aa11099b9c411ba3e59361
Cesar Gomez <zootechdrum@gmail.com>
2020-02-08 11:40:23 -0800

    can now add multiple comments

--- models/Article.js
+++ models/Article.js
@@ -29,10 +29,13 @@ var ArticleSchema = new Schema({
   // `comment` is an object that stores a Note id
   // The ref property links the ObjectId to the Comment model
   // This allows us to populate the Article with an associated Comment
-  comment: {
-    type: Schema.Types.ObjectId,
-    ref: "comment"
-  }
+  comment: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "comment"
+    }
+  ]
+  
 });
 
 // This creates our model from the above schema, using mongoose's model method
--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -102,12 +102,12 @@ app.post("/articles/:id", function(req, res) {
     // Create a new note and pass the req.body to the entry
     db.Comment.create(req.body)
       .then(function(dbComment) {
-          console.log(dbComment)
-        // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-        // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-        // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-        return db.Article.findOneAndUpdate({ _id: req.params.id }, { comment: dbComment._id }, { new: true });
-      })
+          console.log(dbComment._id)
+    //     // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+    //     // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+    //     // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+        return db.Article.findOneAndUpdate({ _id: req.params.id }, { $push: { Comment: dbComment._id } }, { new: true });
+})
       .then(function(dbArticle) {
         // If we were able to successfully update an Article, send it back to the client
         res.json(dbArticle);
@@ -117,5 +117,4 @@ app.post("/articles/:id", function(req, res) {
         res.json(err);
       });
   });
-
 }
