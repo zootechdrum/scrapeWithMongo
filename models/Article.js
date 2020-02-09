6d6eb9f4f89fc9c7c21b78e988ca64bba400fdde
Cesar Gomez <zootechdrum@gmail.com>
2020-02-08 20:06:23 -0800

    added saved paramater to model

--- models/Article.js
+++ models/Article.js
@@ -26,6 +26,11 @@ var ArticleSchema = new Schema({
     type: String,
     required: true
   },
+  saved: {
+    type: Boolean,
+    default: false,
+    required: true
+  },
   // `comment` is an object that stores a Note id
   // The ref property links the ObjectId to the Comment model
   // This allows us to populate the Article with an associated Comment
